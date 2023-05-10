76e399922c0b355b0b0e8e29b0b0f2ceed329e68
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-05-10 23:41:53 +0100

    feat: small fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -51,7 +51,7 @@ export const sections = {
     {
       title: '360 Virtual Tours',
       content: [
-        '360 virtual tours are created by stitching together single pictures to make one unique 360 image.  Commonly used to boost the view experience for existing environments such real estates, retail and hospitality, 360 virtual tours provide a superior immersive way to virtually visit places. Viewers can use their mouse to rotate in all directions or go from one room to another. They can also zoom in on any area within the scene.',
+        '360 virtual tours are created by stitching together single pictures to make one unique 360 image. Commonly used to boost the view experience for existing environments such real estates, retail and hospitality, 360 virtual tours provide a superior immersive way to virtually visit places. Viewers can use their mouse to rotate in all directions or go from one room to another. They can also zoom in on any area within the scene.',
         'A 360 virtual tour is a brilliant interactive way to virtually view a property. Get in touch with Shootime Ltd to shoot and realize your bespoke 360 Virtual Tour.',
       ],
       image: 'image1',
@@ -60,7 +60,7 @@ export const sections = {
     {
       title: 'Matterport',
       content: [
-        'Matterport is the standard for 3D space capture that transforms real-life spaces into immersive digital twin models. So much more than panoramic scans, Matterport empowers people to capture and connect rooms to create truly interactive 3D models of spaces which they can be used for many scopes in different sectors (Real Estate, 3D Photography, Retail, Travel &amp; Hospitality, Insurance &amp; Restoration, Facilities Management, Heritage, Architecture, Engineering &amp; Construction).',
+        'Matterport is the standard for 3D space capture that transforms real-life spaces into immersive digital twin models. So much more than panoramic scans, Matterport empowers people to capture and connect rooms to create truly interactive 3D models of spaces which they can be used for many scopes in different sectors (Real Estate, 3D Photography, Retail, Travel & Hospitality, Insurance & Restoration, Facilities Management, Heritage, Architecture, Engineering & Construction).',
         'Request your Matterport survey contacting Shootime Ltd and we will be happy to discuss your needs and goals, making sure to use the finest equipment and techniques expected for every high-end projects.',
       ],
       image: 'image2',
@@ -69,7 +69,7 @@ export const sections = {
     {
       title: 'Domestic Energy Performance Certificates',
       content: [
-        'Energy Performance Certificates (EPCs) are needed whenever a property is: built, sold and rented. UK Government regulation states you must order an EPC for potential buyers and tenants before you market your property to sell or rent. The EPC will confirm how energy-efficient your property is to date.',
+        'Energy Performance Certificates (EPCs) are needed whenever a property is: built, sold and rented. UK Government regulation states you must order an EPC for potential buyers and tenants before you market your property to sell or rent. The EPC will confirm how energy-efficient your property is to date.',
         'Get in touch with one of our accredited energy assessors at Shootime Ltd to order the EPC for your home and receive recommendations how to improve your home energy rating.',
       ],
       image: 'image3',
