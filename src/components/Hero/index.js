1a202f14bf5fbd0cc1d5dc019560ca25ceb1cdfd
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-06-03 14:40:43 +0100

    handle Hero height

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -15,22 +15,25 @@ const Hero = () => {
           />
         </video>
       </>
-      <h1>We make your world 360</h1>
-      <p>
-        London based company, Shootime Ltd is specialised in 360 photography, 3D
-        digital twins and EPC.
-      </p>
-      <p>
-        Established in 2022 the company has a team of professionals with a
-        strong background in photography, construction, architectural survey and
-        project management.
-      </p>
-      <button>
-        <span>GET A QUOTE</span> <img src={buttonArrow}></img>{' '}
-      </button>
-      <a class="ct-btn-scroll" href="#section-1">
+      <div className="hero-container__content">
+        <h1>We make your world 360</h1>
+        <p>
+          London based company, Shootime Ltd is specialised in 360 photography,
+          3D digital twins and EPC.
+        </p>
+        <p>
+          Established in 2022 the company has a team of professionals with a
+          strong background in photography, construction, architectural survey
+          and project management.
+        </p>
+        <button>
+          <span>GET A QUOTE</span> <img src={buttonArrow}></img>{' '}
+        </button>
+      </div>
+
+      {/* <a class="ct-btn-scroll" href="#section-1">
         <img alt="Arrow Down Icon" src={scrollDown} />
-      </a>
+      </a> */}
     </div>
   )
 }
