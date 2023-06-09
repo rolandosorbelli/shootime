ba9e95a081ebea365707ce19a24bd7c9bd4d4562
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-06-09 23:25:45 +0100

    add new logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,14 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import Navbar from '../Navbar'
 import logo from '../../images/shootime2.svg'
+import noSlogan from '../../images/no-slogan.svg'
 
 import './index.scss'
 
 const Header = ({ siteTitle }) => (
   <header className="primary-header flex">
     <Link to="/">
-      <img src={logo} alt={`${siteTitle} logo`} />
+      <img src={noSlogan} alt={`${siteTitle} logo`} />
     </Link>
 
     <Navbar />
