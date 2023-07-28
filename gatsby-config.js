ed18d7ec59ff7d4cc8a930f10435d566fcb632ac
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-07-28 16:15:04 +0100

    change icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/shootime.svg`, // This path is relative to the root of the site.
+        icon: `src/images/shootime4.svg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
