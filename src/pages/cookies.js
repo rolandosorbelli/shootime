ae5be6714d0a96713fe2b48a0c1474c07122c658
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-07-30 12:45:39 +0100

    change cookies page

--- src/pages/cookies.js
+++ src/pages/cookies.js
@@ -28,203 +28,177 @@ const Cookies = ({ children }) => {
       >
         <main>
           <div className="policy-outer">
-            <h1>Cookie Policy for Shootime Ltd</h1>
+            <h1>About cookies</h1>
             <p>
-              This is the Cookie Policy for Shootime Ltd, accessible from
-              www.shootime.co.uk
+              Cookies are a kind of short-term memory for the web. They are
+              stored in your browser and enable a site to ‘remember’ little bits
+              of information between pages or visits.
             </p>
             <p>
-              <strong>What Are Cookies</strong>
+              Cookies can be used by web servers to identify and track users as
+              they navigate different pages on a website, and to identify users
+              returning to a website. Cookies may be either “persistent” cookies
+              or “session” cookies. A persistent cookie consists of a text file
+              sent by a web server to a web browser, which will be stored by the
+              browser and will remain valid until its set expiry date (unless
+              deleted by the user before the expiry date). A session cookie, on
+              the other hand, will expire at the end of the user session, when
+              the web browser is closed.
+            </p>
+            <h2>Cookies on our website</h2>
+            <p>
+              The cookies used on this website include those which are strictly
+              necessary cookies for access and navigation, cookies that track
+              usage (performance cookies) and cookies used to allow you to log
+              into our bookings section, if we have allowed you access.
             </p>
             <p>
-              As is common practice with almost all professional websites this
-              site uses cookies, which are tiny files that are downloaded to
-              your computer, to improve your experience. This page describes
-              what information they gather, how we use it and why we sometimes
-              need to store these cookies. We will also share how you can
-              prevent these cookies from being stored however this may downgrade
-              or 'break' certain elements of the sites functionality.
-            </p>
-            <p>
-              <strong>How We Use Cookies</strong>
-            </p>
-            <p>
-              We use cookies for a variety of reasons detailed below.
-              Unfortunately in most cases there are no industry standard options
-              for disabling cookies without completely disabling the
-              functionality and features they add to this site. It is
-              recommended that you leave on all cookies if you are not sure
-              whether you need them or not in case they are used to provide a
-              service that you use.
-            </p>
-            <p>
-              <strong>Disabling Cookies</strong>
-            </p>
-            <p>
-              You can prevent the setting of cookies by adjusting the settings
-              on your browser (see your browser Help for how to do this). Be
-              aware that disabling cookies will affect the functionality of this
-              and many other websites that you visit. Disabling cookies will
-              usually result in also disabling certain functionality and
-              features of the this site. Therefore it is recommended that you do
-              not disable cookies. This Cookies Policy was created with the help
-              of the{' '}
-              <a href="https://www.cookiepolicygenerator.com/cookie-policy-generator/">
-                Cookies Policy Generator
-              </a>
-              .
-            </p>
-            <p>
-              <strong>The Cookies We Set</strong>
+              We may use the information we obtain from your use of our cookies
+              for the following purposes:
             </p>
             <ul>
               <li>
-                <p>Email newsletters related cookies</p>
-                <p>
-                  This site offers newsletter or email subscription services and
-                  cookies may be used to remember if you are already registered
-                  and whether to show certain notifications which might only be
-                  valid to subscribed/unsubscribed users.
-                </p>
+                to recognise your computer when you visit our website and log
+                into our bookings section.
               </li>
+              <li>to track you as you navigate our website.</li>
+              <li>to improve the website’s usability.</li>
               <li>
-                <p>Orders processing related cookies</p>
-                <p>
-                  This site offers e-commerce or payment facilities and some
-                  cookies are essential to ensure that your order is remembered
-                  between pages so that we can process it properly.
-                </p>
-              </li>
-              <li>
-                <p>Surveys related cookies</p>
-                <p>
-                  From time to time we offer user surveys and questionnaires to
-                  provide you with interesting insights, helpful tools, or to
-                  understand our user base more accurately. These surveys may
-                  use cookies to remember who has already taken part in a survey
-                  or to provide you with accurate results after you change
-                  pages.
-                </p>
-              </li>
-              <li>
-                <p>Forms related cookies</p>
-                <p>
-                  When you submit data to through a form such as those found on
-                  contact pages or comment forms cookies may be set to remember
-                  your user details for future correspondence.
-                </p>
-              </li>
-              <li>
-                <p>Site preferences cookies</p>
-                <p>
-                  In order to provide you with a great experience on this site
-                  we provide the functionality to set your preferences for how
-                  this site runs when you use it. In order to remember your
-                  preferences we need to set cookies so that this information
-                  can be called whenever you interact with a page is affected by
-                  your preferences.
-                </p>
+                to analyse the use of our website – such as how many people
+                visit us each day (see Google Analytics section below).
               </li>
             </ul>
             <p>
-              <strong>Third Party Cookies</strong>
+              <strong>Third party cookies</strong>
             </p>
             <p>
-              In some special cases we also use cookies provided by trusted
-              third parties. The following section details which third party
-              cookies you might encounter through this site.
+              We work with the following company in maintaining and improving
+              our website. A brief description of what they do is found below.
+              We ensure that any company which works with us and collects
+              personal data complies with strict GDPR data protection standards.
+              If you have any more questions, please do contact us and we’ll be
+              happy to help.
+            </p>
+            <h2>Google & Google Analytics</h2>
+            <p>
+              We use Google Analytics to better understand how our customers
+              navigate to and through our website, how long customers spend
+              visiting our content items and how often they return to visit our
+              website. Google Analytics also helps us track the effectiveness of
+              the money we spend on our digital marketing. Google’s GDPR
+              compliant Privacy Policy, which contains information on cookies,
+              can be found here (https://policies.google.com/). We have blocked
+              Google Analytics from recording IP addresses. This means we do not
+              collect any personal information through the use of cookies on
+              this website and have removed any services (such as social media
+              plugins) that do.
+            </p>
+            <h2>Blocking cookies</h2>
+            <p>
+              Most browsers allow you to refuse to accept cookies. For example:
             </p>
             <ul>
               <li>
-                <p>
-                  This site uses Google Analytics which is one of the most
-                  widespread and trusted analytics solution on the web for
-                  helping us to understand how you use the site and ways that we
-                  can improve your experience. These cookies may track things
-                  such as how long you spend on the site and the pages that you
-                  visit so we can continue to produce engaging content.
-                </p>
-                <p>
-                  For more information on Google Analytics cookies, see the
-                  official Google Analytics page.
-                </p>
+                In Internet Explorer 11 you can refuse all cookies by clicking
+                “Tools”, “Internet Options”, select the “Privacy” tab and,
+                locate “Advanced” under Settings and choose if you want to
+                allow, block or be promoted for first and third-party cookies.
               </li>
               <li>
-                <p>
-                  Third party analytics are used to track and measure usage of
-                  this site so that we can continue to produce engaging content.
-                  These cookies may track things such as how long you spend on
-                  the site or pages you visit which helps us to understand how
-                  we can improve the site for you.
-                </p>
+                In Microsoft Edge click on the three dots icon in the top
+                right-hand corner and select “Settings”. Locate “View advanced
+                settings”. In the “Cookies” section you will find a drop- down
+                menu where you can select “Block only third-party cookies” or
+                “Block all cookies”. You will then have to re-start Microsoft
+                Edge.
               </li>
               <li>
-                <p>
-                  From time to time we test new features and make subtle changes
-                  to the way that the site is delivered. When we are still
-                  testing new features these cookies may be used to ensure that
-                  you receive a consistent experience whilst on the site whilst
-                  ensuring we understand which optimisations our users
-                  appreciate the most.
-                </p>
+                In Firefox you can block all cookies by clicking “Tools”,
+                “Options”, and un- checking “Accept cookies from sites” in the
+                “Privacy” box.
               </li>
               <li>
-                <p>
-                  The Google AdSense service we use to serve advertising uses a
-                  DoubleClick cookie to serve more relevant ads across the web
-                  and limit the number of times that a given ad is shown to you.
-                </p>
-                <p>
-                  For more information on Google AdSense see the official Google
-                  AdSense privacy FAQ.
-                </p>
+                In Google Chrome you can adjust your cookie permissions by
+                clicking on the three dots icon in the top right-hand corner of
+                your browser screen, selecting “Settings” from near the bottom
+                of the drop-down list, then clicking on “Advanced” near the
+                bottom. This should open options. In the “Privacy” section,
+                click “Content Settings” and disable cookies by selecting “Block
+                site from setting any data”. Click “Done” to save your
+                preferences.
               </li>
               <li>
-                <p>
-                  Several partners advertise on our behalf and affiliate
-                  tracking cookies simply allow us to see if our customers have
-                  come to the site through one of our partner sites so that we
-                  can credit them appropriately and where applicable allow our
-                  affiliate partners to provide any bonus that they may provide
-                  you for making a purchase.
-                </p>
+                On Safari on Apple OS X click on the Safari menu, then
+                “Preferences” then “Privacy”. Under “Accept Cookies” unselect
+                the “Always allow” option.
               </li>
               <li>
-                <p>
-                  We also use social media buttons and/or plugins on this site
-                  that allow you to connect with your social network in various
-                  ways. For these to work the following social media sites
-                  including; &#123;List the social networks whose features you
-                  have integrated with your site?:12&#125;, will set cookies
-                  through our site which may be used to enhance your profile on
-                  their site or contribute to the data they hold for various
-                  purposes outlined in their respective privacy policies.
-                </p>
+                For Opera choose “Settings”, “Quick Preferences” and uncheck
+                “Enable Cookies”.
               </li>
             </ul>
-            <p>
-              <strong>More Information</strong>
-            </p>
-            <p>
-              Hopefully that has clarified things for you and as was previously
-              mentioned if there is something that you aren't sure whether you
-              need or not it's usually safer to leave cookies enabled in case it
-              does interact with one of the features you use on our site.
-            </p>
-            <p>
-              For more general information on cookies, please read{' '}
-              <a href="https://www.cookiepolicygenerator.com/sample-cookies-policy/">
-                the Cookies Policy article
-              </a>
-              .
-            </p>
-            <p>
-              However if you are still looking for more information then you can
-              contact us through one of our preferred contact methods:
-            </p>
+            <h2>Deleting cookies</h2>
+            <p>You can also delete cookies already stored on your computer:</p>
             <ul>
-              <li>Email: studio@shootime.co.uk</li>
+              <li>
+                In Internet Explorer 11 click on “Tools”, point to “Safety” and
+                then select the “Delete browsing history”. Click on the “Cookies
+                and website data” checkbox and then choose “Delete”.
+              </li>
+              <li>
+                In Microsoft Edge click on the three dots icon in the top
+                right-hand corner and select “Settings”. Under “Clear browsing
+                data” select “Choose what to clear” and select the checkbox next
+                to “Cookies and saved website data” then click on “Clear”. If
+                you wish you can turn the “Always clear this when I close the
+                browser” setting to “On”.
+              </li>
+              <li>
+                To delete cookies from a particular website in Firefox, click on
+                the three lines icon and choose “Options”. In the “Privacy &amp;
+                Security” panel go to the “History” section and in the drop menu
+                next to “Firefox will” choose “Use custom settings for history”.
+                A cookies window will appear when you click “Show Cookies”. In
+                the Search field type the name of the site whose cookies you
+                want to remove. Select the cookies you wish to remove and click
+                “Remove Selected”. Click “Close” to close the Cookies window and
+                the “about:preferences” page. To delete all cookies, open the
+                cookies window as above and click “Remove All Cookies”.
+              </li>
+              <li>
+                In Google Chrome click on the three dots icon in the top
+                right-hand corner, select “Settings” and find “Advanced” near
+                the bottom. Under “Privacy and security” click on “Content
+                settings”. Click on “Cookies” and Under “All cookies and site
+                data” select “Remove All”. Confirm by clicking on “Clear all”.
+              </li>
+              <li>
+                On Safari on Apple OS X click on the Safari menu, then select
+                “Preferences” from the drop- down. Go to the “Privacy” tab.
+                Click the “Remove All Website Data” button to remove all stored
+                website data and click “Remove Now” to confirm. To remove stored
+                data by individual site, click on the “Details” button and
+                select the sites with the stored data you wish to remove. Click
+                “Remove” and then “Done”.
+              </li>
+              <li>
+                For Opera selecting “Settings”, “Preferences”, “Advanced” and
+                then “Cookies” and checking the “Delete new cookies when exiting
+                Opera” and closing Opera when you have finished browsing will
+                ensure the cookie information is kept in the browser.
+              </li>
             </ul>
+            <p>
+              Doing this may limit the usability of many websites. Please do not
+              hesitate to get in touch with us using the contact details on our
+              contact page if you have any questions or queries regarding how we
+              use cookies or anything else on our website.
+            </p>
+            <h2>Questions</h2>
+            <p>
+              If you have any questions about our Policy or your own personal
+              information, please send an email to studio@shootime.co.uk
+            </p>
           </div>
         </main>
       </div>
