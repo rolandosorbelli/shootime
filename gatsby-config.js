99a6c507a239417f472478fb22b5b898253aaa76
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2023-08-24 12:17:53 +0100

    add Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,5 +93,19 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: ['G-SL2JZKK8FN'],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+      },
+    },
   ],
 }
