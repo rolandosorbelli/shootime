3c87beca81a2b7b90168e3bc832c6ac75e3cf8fc
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2024-06-09 19:25:31 +0100

    add logos and tweak instagram

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import linkedin from '../../images/linkedin-icon.svg'
+import instagram from '../../images/instagram-icon.svg'
+import logo1 from '../../images/footer-iguide.png'
+import logo2 from '../../images/footer-matterport.png'
+import logo3 from '../../images/footer-elmhurst.png'
+import logo4 from '../../images/footer-cng.png'
 
 import './index.scss'
 
@@ -11,9 +16,22 @@ const Footer = ({}) => {
           <h3>Shootime</h3>
           <p>86-90 Paul Street, London, EC2A 4NE</p>
           <p>Company Number: 14429115</p>
-          <a href="https://www.linkedin.com/company/shootime-ltd/">
-            <img src={linkedin} alt="LinkedIn page" className="linkedin"></img>
-          </a>
+          <div className="footer-upper__left-logos">
+            <a
+              href="https://www.linkedin.com/company/shootime-ltd/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="LinkedIn page" className="linkedin" />
+            </a>
+            <a
+              href="https://www.instagram.com/shootime_ltd"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instagram} alt="Instagram page" className="instagram" />
+            </a>
+          </div>
         </div>
         <div className="footer-upper__right">
           <div className="footer-upper__right-company">
@@ -50,6 +68,15 @@ const Footer = ({}) => {
             </p>
           </div>
         </div>
+      </div>
+      <div className="footer-center">
+        <img src={logo1} alt="iGuide Logo" />
+        <img src={logo2} alt="Matterport Logo" />
+        <img src={logo3} alt="Elmhurst energy Logo" />
+        <img
+          src={logo4}
+          alt="Consiglio Nazionale Geometri e Geometri Laureati Logo"
+        />
       </div>
       <div className="footer-lower">
         <p>&copy; Copyright {new Date().getFullYear()}, Shootime.co.uk</p>
